8c8ce9cd8a9feb59e6eda2861489e451f7f326f7
upokusaev <upokusaev@gmail.com>
2019-12-01 19:22:19 +0400

    Реализовал общий редьюсер

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import channelsReducer from '../features/channels/channelsSlice.jsx';
 import messagesReducer from '../features/messages/messagesSlice.jsx';
+import modalsReducer from '../features/modals/modalsSlice.jsx';
 
 export default combineReducers({
   channels: channelsReducer,
-  messages: messagesReducer
+  messages: messagesReducer,
+  modals: modalsReducer,
 });
