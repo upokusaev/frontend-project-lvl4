6a5f8cf7bf209fabc7a4df521a8317c892b4c77a
upokusaev <upokusaev@gmail.com>
2019-12-01 19:12:20 +0400

    Перенес сокеты в App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,38 +2,62 @@ import React from 'react';
 import { render } from 'react-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import io from 'socket.io-client';
+import AddChannel from '../features/channels/AddChannel.jsx';
 import ListChannels from '../features/channels/ListChannels.jsx';
 import ListMessages from '../features/messages/ListMessages.jsx';
-import AddMessage from '../features/messages/AddMessage.jsx'
+import AddMessage from '../features/messages/AddMessage.jsx';
+import ModalRoot from '../features/modals/ModalRoot.jsx';
+import UserNameContext from '../conexts/userNameContext.jsx';
 import rootReducer from '../reducers';
-// import { addMessage } from '../features/messages/messagesSlice.jsx'
-// import io from 'socket.io-client';
+import { actions as actionsChannels } from '../features/channels/channelsSlice.jsx';
+import { actions as actionsMessages } from '../features/messages/messagesSlice.jsx';
 
 const store = configureStore({
-  reducer: rootReducer
-})
+  reducer: rootReducer,
+});
 
-export const UserNameContext = React.createContext();
+const { addChannel, removeChannel, renameChannel } = actionsChannels;
+const { addMessage } = actionsMessages;
+
+const socket = io();
+socket.on('connect', () => {
+  socket.on('newMessage', (data) => {
+    store.dispatch(addMessage(data));
+  });
+
+  socket.on('newChannel', (data) => {
+    store.dispatch(addChannel(data));
+  });
+
+  socket.on('removeChannel', (data) => {
+    store.dispatch(removeChannel(data));
+  });
+
+  socket.on('renameChannel', (data) => {
+    store.dispatch(renameChannel(data));
+  });
+});
+
+const App = () => (
+  <div className="d-flex flex-row border rounded h-100 shadow-lg">
+    <ListChannels>
+      <AddChannel />
+    </ListChannels>
+    <ListMessages>
+      <AddMessage />
+    </ListMessages>
+  </div>
+);
 
 export default (userName) => {
-
-  const App = () => {
-    return (
-      <div className="d-flex flex-row border rounded h-100 shadow-lg">
-        <ListChannels/>
-        <ListMessages>
-          <AddMessage/>
-        </ListMessages>
-      </div>
-    );
-  };
-
   render(
     <Provider store={store}>
       <UserNameContext.Provider value={userName}>
-        <App/>
+        <App />
+        <ModalRoot />
       </UserNameContext.Provider>
     </Provider>,
-    document.getElementById('chat')
+    document.getElementById('chat'),
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/application.css';
-import app from './components/App.jsx';
-import io from 'socket.io-client';
-
-// import { addMessage } from './features/messages/AddMessage.jsx'
 import faker from 'faker';
-// import gon from 'gon';
 import cookies from 'js-cookie';
+import app from './components/App.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-faker.locale = "ru";
+faker.locale = 'ru';
 
 const setUserName = () => {
   const newUserName = faker.name.findName();
   cookies.set('userName', newUserName);
   return newUserName;
-}
+};
 
 const getUserName = () => {
   const cookiesName = cookies.get('userName');
-  return cookiesName ? cookiesName : setUserName();
-}
+  return cookiesName || setUserName();
+};
 
 app(getUserName());
-
-const socket = io();
-socket.on('connect', () => {
-  console.log('Подключились');
-  socket.on('disconnect', () => {
-    console.log('Отключились');
-  });
-});
